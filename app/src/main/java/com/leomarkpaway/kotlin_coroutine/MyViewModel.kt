2d9bac2ebbb1d220b3e6cd796dfc60a92d5c68d3
leomarkpaway <leomarkpaway@gmail.com>
2024-08-16 12:45:28 +0800

    :sparkles: Create sample return value from coroutine scope

--- app/src/main/java/com/leomarkpaway/kotlin_coroutine/MyViewModel.kt
+++ app/src/main/java/com/leomarkpaway/kotlin_coroutine/MyViewModel.kt
@@ -3,6 +3,8 @@ package com.leomarkpaway.kotlin_coroutine
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MyViewModel : ViewModel() {
@@ -11,6 +13,17 @@ class MyViewModel : ViewModel() {
         viewModelScope.launch {
             // viewModelScope is aware on view model life cycle
             Log.d("viewModelScope", this.coroutineContext.toString())
+        }
+
+        // Demo: return value from coroutine scope
+        val result = viewModelScope.async {
+            delay(3000)
+            "result value"
+        }
+        result.invokeOnCompletion {
+            if(it == null) {
+                Log.d( "returnValue", result.getCompleted())
+            }
         }
     }
 
